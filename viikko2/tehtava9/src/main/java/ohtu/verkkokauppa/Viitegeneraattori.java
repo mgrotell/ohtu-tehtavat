46958966daceb9fc329555bed5e814805786376e
Miklas Siivonen <msiivone@student.hive.fi>
2021-11-15 12:53:57 +0200

    Tehtävä 12 toteutettu tehtävän 9 sisään.

--- viikko2/tehtava9/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
+++ viikko2/tehtava9/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -1,11 +1,16 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Viitegeneraattori implements ViiteheneraattoriInterface {
 
     private static Viitegeneraattori instanssi;
     
     private int seuraava;
     
+    @Autowired
     public Viitegeneraattori(){
         this.seuraava = 1;    
     }
